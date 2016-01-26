e27ae8cb90b3159af8d7c3142c76488971b97432
personal <personal@personal-PC>
2016-01-25 16:49:45 -0800

    1st commit --- details

--- src/sampleintern/SampleIntern.java
+++ src/sampleintern/SampleIntern.java
@@ -17,6 +17,7 @@ public class SampleIntern {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("new codes from janica...");
     }
     
 }
