4a1640707552735ef9aae8b3c1ae785627095f1d
Cordura <Cordura@192.168.1.57>
2016-01-26 16:35:46 -0800

    2nd

--- src/sampleintern/SampleIntern.java
+++ src/sampleintern/SampleIntern.java
@@ -17,7 +17,7 @@ public class SampleIntern {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Peter Sulima...Kenneth ARcinue...Acinaj");
+        System.out.println("c kenneth kay bayot");
         System.out.println("hello world");
         
         
